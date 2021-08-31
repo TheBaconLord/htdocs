73a1261609402082c9c8c3e82f8a9699955f05e7
TheBaconLord <sebastianhauglid@gmail.com>
2021-08-31 05:38:53 -0700

    New oblig 2

---
+++ 2/Index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="main2.css">
+    <title>Oblig 2</title>
+
+</head>
+<body>
+<div class="header">
+  <h1>BMI kalkulator</h1>
+</div>
+
+
+ <div class="footer">
+   <p id="sebastianhauglid">Sebastian Hauglid</p>
+ </div>
+ <textarea 
+ name="Comentar" id="text" cols="50" rows="15">
+ </textarea>
+</body>
+</html>
