db6b993591e3a27d2a041f340e442a1a940ef420
TheBaconLord <sebastianhauglid@gmail.com>
2021-10-28 05:50:17 -0700

    start på oblig7 oppgave1

---
+++ 1/oblig7/oppgave1/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../../main.css">
+    <link rel="stylesheet" href="../oblig7.css">
+    <link rel="stylesheet" href="oppgave1.css">
+    <title>Oppgave 1</title>
+</head>
+<body>
+    <div class="header">
+        <h1>Oppgave 1</h1>
+    </div>
+    <div class="topnav">
+        <a href="../">Tilbake</a>
+        <a href="../../">Til forside</a>
+    </div>
+    <div  id="content">
+        <h2>oppgave 1</h2>
+            <ol class= "abc">
+                <li>
+                    <p>
+                        Forskjellen på en "Class" og et "Objekt" er at, en Class er en mal for et objekt mens et objekt er et eksempel av en class.
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        Man kan inkludere en ny klasse på main-PHP siden sin ved å lage en ny class.
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        Nøkkelordet som blir brukt for å lage et nytt objekt er "new".
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        Den vanlgiste funksjonen å bruke når man skal endre et objekts egenskaper er "set_".
+                    </p>
+                </li>
+                <li>
+                    <p>
+                    Den vanlgiste funksjonen å bruke når man skal endre et objekts egenskaper er "get_".
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        En konstruktor er en funksjon som lar deg initialisere et objekts egenskaper når man lager objektet. 
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        
+                    </p>
+                </li>
+            </ol>
+    </div>
+    <div class="footer">
+        <p id="sebastianhauglid">Sebastian Hauglid</p>
+    </div>
+</body>
+</html>
---
+++ 1/oblig7/oppgave2/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../../main.css">
+    <link rel="stylesheet" href="../oblig7.css">
+    <link rel="stylesheet" href="oppgave2.css">
+    <title>Oppgave 2</title>
+</head>
+<body>
+    <div class="header">
+        <h1>Oppgave 2</h1>
+    </div>
+    <div class="topnav">
+        <a href="../">Tilbake</a>
+        <a href="../../">Til forside</a>
+    </div>
+    <div  id="content">
+        <h2>oppgave 2</h2>
+            <ol class= "abc">
+                <li>
+                    <p>
+
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        
+                    </p>
+                </li>
+                
+            </ol>
+    </div>
+    <div class="footer">
+        <p id="sebastianhauglid">Sebastian Hauglid</p>
+    </div>
+</body>
+</html>
